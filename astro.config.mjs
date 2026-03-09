66bf75ad62e7819dee85a07ec632fb261874ac28
diaz web app <diazwebapp@gmail.com>
2026-03-09 15:43:05 +0000

    modificado astro.config, no quiere compilar

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  output:'server',
     image: {
     // Esto permite que Astro optimice imágenes de estos sitios
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mmreyesdelnorte.com', // El dominio donde están las fotos
+        pathname: '/images/**',     // Opcional: limitar a una carpeta específica
+      },
       {
         protocol: 'https',
         hostname: 'astro.build',
