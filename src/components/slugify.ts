ad2c170621380d2420b601f909a0df715540e52c
diaz web app <diazwebapp@gmail.com>
2026-03-09 02:34:35 +0000

    añadiendo schemas y mejorando los slugs

--- src/components/slugify.ts
+++ src/components/slugify.ts
@@ -1,5 +1,9 @@
+const BASE_URL = import.meta.env.DEV 
+  ? 'https://fuzzy-space-orbit-67p4jqjrxr7257vq.github.dev/' 
+  : 'https://mmreyesdelnorte.com/';
+
 export function slugify(text: string): string {
-  return text
+  return BASE_URL + text
     .toString()
     .normalize('NFD') // Separa las letras de los acentos (e.g., "é" -> "e" + "´")
     .replace(/[\u0300-\u036f]/g, '') // Elimina los acentos (diacríticos)
@@ -7,5 +11,6 @@ export function slugify(text: string): string {
     .trim()
     .replace(/\s+/g, '-')           // Reemplaza espacios con -
     .replace(/[^\w\-]+/g, '')       // Elimina caracteres no válidos
-    .replace(/\-\-+/g, '-');        // Reemplaza múltiples guiones con uno solo
+    .replace(/\-\-+/g, '-')       // Reemplaza múltiples guiones con uno solo
+    + '/'; // Agrega una barra al final para formar la URL completa
 }
--- src/local-data/database.ts
+++ src/local-data/database.ts
@@ -2,7 +2,7 @@ import { slugify } from "../components/slugify";
 import type { Categoria, Producto } from "./database.types";
 
 const BASE_URL = import.meta.env.DEV 
-  ? '' 
+  ? 'https://fuzzy-space-orbit-67p4jqjrxr7257vq.github.dev' 
   : 'https://mmreyesdelnorte.com';
 
 export const categorias: Categoria[] = [
@@ -10,49 +10,49 @@ export const categorias: Categoria[] = [
       "id": "cat_1",
       "nombre": "Mini Dumper Diésel",
       "slug": slugify("Mini Dumper Diésel"),
-      "imagen": "/images/categorias/mini-dumper-diesel.webp"
+      "imagen": BASE_URL+"/images/categorias/mini-dumper-diesel.webp"
     },
     {
       "id": "cat_2",
       "nombre": "Mini Dumper Eléctrico",
       "slug": slugify("Mini Dumper Eléctrico"),
-      "imagen": "/images/categorias/mini-dumper-electrico.webp"
+      "imagen": BASE_URL+"/images/categorias/mini-dumper-electrico.webp"
     },
     {
       "id": "cat_3",
       "nombre": "Scoop / Mini Cargador",
       "slug": slugify("scoop-mini-cargador"),
-      "imagen": "/images/categorias/scoop.webp"
+      "imagen": BASE_URL+"/images/categorias/scoop.webp"
     },
     {
       "id": "cat_4",
       "nombre": "Mucking Loader",
       "slug": slugify("Mucking Loader"),
-      "imagen": "/images/categorias/mucking-loader.webp"
+      "imagen": BASE_URL+"/images/categorias/mucking-loader.webp"
     },
     {
       "id": "cat_5",
       "nombre": "Grupos Electrógenos",
       "slug": slugify("grupos Electrogenos"),
-      "imagen": "/images/categorias/electrogenos.webp"
+      "imagen": BASE_URL+"/images/categorias/electrogenos.webp"
     },
     {
       "id": "cat_6",
       "nombre": "Perforación y Arrastre",
       "slug": slugify("perforacion y Arrastre"),
-      "imagen": "/images/categorias/arrastre.png"
+      "imagen": BASE_URL+"/images/categorias/arrastre.png"
     },
     {
       "id": "cat_7",
       "nombre": "Ventilación Industrial",
       "slug": slugify("Ventilación Industrial"),
-      "imagen": "/images/categorias/ventiladores.webp"
+      "imagen": BASE_URL+"/images/categorias/ventiladores.webp"
     },
     {
       "id": "cat_8",
       "nombre": "Carretillas Eléctricas",
       "slug": slugify("Carretillas Eléctricas"),
-      "imagen": "/images/categorias/carretilla.webp"
+      "imagen": BASE_URL+"/images/categorias/carretilla.webp"
     }
   ]
   
@@ -77,7 +77,7 @@ export const productos: Producto[] = [
       "categoria_id": "cat_1",
       "modelo_o_capacidad": "2.5 TN CV900",
       "slug": slugify("Mini Dumper Diésel 2.5 TN CV900"),
-      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-diesel-1.5tn.png",
+      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-diesel-2.5tn.png",
       "motor": {
         "tipo": "Diésel",
         "potencia": "28 HP",
@@ -91,7 +91,7 @@ export const productos: Producto[] = [
       "categoria_id": "cat_1",
       "modelo_o_capacidad": "3.5 TN",
       "slug": slugify("Mini Dumper Diésel 3.5 TN"),
-      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-diesel-1.5tn.png",
+      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-diesel-3.5tn.png",
       "motor": {
         "tipo": "Diésel",
         "potencia": "32 HP"
@@ -105,7 +105,7 @@ export const productos: Producto[] = [
       "categoria_id": "cat_1",
       "modelo_o_capacidad": "5-6 TN",
       "slug": slugify("Mini Dumper Diésel 5-6 TN"),
-      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-diesel-1.5tn.png",
+      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-diesel-6tn.png",
       "motor": {
         "tipo": "Diésel - Yunnei 490 Turbo",
         "potencia": "75 HP",
@@ -119,7 +119,7 @@ export const productos: Producto[] = [
       "categoria_id": "cat_1",
       "modelo_o_capacidad": "7-8 TN",
       "slug": slugify("Mini Dumper Diésel 7-8 TN"),
-      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-diesel-1.5tn.png",
+      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-diesel-8tn.png",
       "motor": {
         "tipo": "Diésel - Yunnei 4102 Turbo",
         "potencia": "102 HP",
@@ -134,7 +134,7 @@ export const productos: Producto[] = [
       "categoria_id": "cat_2",
       "modelo_o_capacidad": "1 TN",
       "slug": slugify("Mini Dumper Eléctrico 1 TN"),
-      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-diesel-1.5tn.png",
+      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-electrico-1tn.png",
       "motor": {
         "tipo": "Eléctrico",
         "potencia": "3000W"
@@ -148,7 +148,7 @@ export const productos: Producto[] = [
       "categoria_id": "cat_2",
       "modelo_o_capacidad": "1.5 TN",
       "slug": slugify("Mini Dumper Eléctrico 1.5 TN"),
-      "imagen_portada": "/images/productos/mini-dumper-diesel-1.5tn.webp",
+      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-electrico-1.5tn.webp",
       "motor": {
         "tipo": "Eléctrico",
         "potencia": "3000W"
@@ -161,7 +161,7 @@ export const productos: Producto[] = [
       "categoria_id": "cat_2",
       "modelo_o_capacidad": "1.5 TN Sistema datai",
       "slug": slugify("Mini Dumper Eléctrico 1.5 TN Sistema datai"),
-      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-diesel-1.5tn.png",
+      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-electrico-1.5tn.webp",
       "motor": {
         "tipo": "Eléctrico",
         "potencia": "4000W Sistema hermético a prueba de agua y Sistema de 6 fases."
@@ -175,7 +175,7 @@ export const productos: Producto[] = [
       "categoria_id": "cat_3",
       "modelo_o_capacidad": "GDDC - 300 KG.",
       "slug": slugify("Mini Cargador Eléctrico GDDC 300 KG"),
-      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-diesel-1.5tn.png",
+      "imagen_portada": BASE_URL+"/images/productos/Mini-Cargador-Electrico-300kg-GDDC.png",
       "tipo": "Mini Cargador Eléctrico",
       "motor": {
         "tipo": "Eléctrico",
@@ -189,7 +189,7 @@ export const productos: Producto[] = [
       "categoria_id": "cat_3",
       "modelo_o_capacidad": "CHWJ-0.6",
       "slug": slugify("Scoop CHWJ-0.6"),
-      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-diesel-1.5tn.png",
+      "imagen_portada": BASE_URL+"/images/productos/scoops-chwj-06.png",
       "tipo": "Scoop",
       "motor": {
         "modelo": "DEUTZ BF4L914",
@@ -203,7 +203,7 @@ export const productos: Producto[] = [
       "categoria_id": "cat_3",
       "modelo_o_capacidad": "CHWJ-1",
       "slug": slugify("Scoop CHWJ-1"),
-      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-diesel-1.5tn.png",
+      "imagen_portada": BASE_URL+"/images/productos/scoops-chwj-06.png",
       "tipo": "Scoop",
       "motor": {
         "modelo": "DEUTZ BF4L914",
@@ -215,9 +215,9 @@ export const productos: Producto[] = [
     {
       "id": "prod_12",
       "categoria_id": "cat_4",
-      "modelo_o_capacidad": "50/60T",
-      "slug": slugify("Mucking Loader 50-60T"),
-      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-diesel-1.5tn.png",
+      "modelo_o_capacidad": "50/60TN",
+      "slug": slugify("Mucking Loader 50-60TN"),
+      "imagen_portada": BASE_URL+"/images/productos/mucking-loader-60tn.png",
       "eficiencia": "60m³/hora",
       "motor": {
         "diesel": "Changchal ZS1115 (24HP)",
@@ -230,7 +230,7 @@ export const productos: Producto[] = [
       "categoria_id": "cat_5",
       "modelo_o_capacidad": "10KW",
       "slug": slugify("Generador Diésel 10KW"),
-      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-diesel-1.5tn.png",
+      "imagen_portada": BASE_URL+"/images/productos/Generador-Diesel-10KW.png",
       "motor": {
         "marca": "Isuzu",
         "rpm": "1800 RPM"
@@ -243,7 +243,7 @@ export const productos: Producto[] = [
       "categoria_id": "cat_5",
       "modelo_o_capacidad": "30KW",
       "slug": slugify("Generador Diésel 30KW"),
-      "imagen_portada": BASE_URL+"/images/productos/mini-dumper-diesel-1.5tn.png",
+      "imagen_portada": BASE_URL+"/images/productos/Generador-Diesel-30KW.png",
       "motor": {
         "marca": "Perkinks",
         "modelo": "1004G"
@@ -260,7 +260,7 @@ export const productos: Producto[] = [
       "tipo": "Neumática",
       "peso": "29-32 Kg",
       "consumo_aire": "565 L/s",
-      "profundidad_max_taladro": "6 m."
+      "profundidad_max_taladro": "6m"
     },
     {
       "id": "prod_16",
