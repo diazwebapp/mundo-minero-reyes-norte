4bb31fa36cbde451a986dcc7014cdfd45b3bd4c9
diazwebapp <diazwebapp@gmail.com>
2026-03-24 23:40:28 +0000

    añadiendo modificando og:image

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,20 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output:'server',
+  site: 'https://mundo-minero-reyes-del-norte.vercel.app',
     image: {
-    // Esto permite que Astro optimice imágenes de estos sitios
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'mmreyesdelnorte.com', // El dominio donde están las fotos
-        pathname: '/**',     // Opcional: limitar a una carpeta específica
+        hostname: 'mmreyesdelnorte.com',
       },
       {
         protocol: 'https',
-        hostname: 'astro.build',
+        hostname: '*.supabase.co', // Importante para que Astro procese las fotos del storage
+      },
+       {
+        protocol: 'https',
+        hostname: '*.vercel.app', // Importante para que Astro procese las fotos del storage
       },
     ],
   },
