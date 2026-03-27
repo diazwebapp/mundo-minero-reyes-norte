331125579f3150c79bdb681406abe53b9e3400b1
diazwebapp <diazwebapp@gmail.com>
2026-03-27 03:34:41 +0000

    actualizado el menu de navegacion, seccion de repuestos y se soluciono el tema de canibalizacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-
 import { defineConfig } from 'astro/config';
 import { loadEnv } from 'vite';
 import vercel from '@astrojs/vercel';
+
+import icon from 'astro-icon';
 
 const {DOMAIN} = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
 
@@ -9,21 +10,24 @@ const {DOMAIN} = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '
 export default defineConfig({
   output:'server',
   site: DOMAIN,
-    image: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: DOMAIN,
-      },
-      {
-        protocol: 'https',
-        hostname: '*.supabase.co', // Importante para que Astro procese las fotos del storage
-      },
-       {
-        protocol: 'https',
-        hostname: '*.vercel.app', // Importante para que Astro procese las fotos del storage
-      },
-    ],
-  },
-  adapter: vercel()
+
+  image: {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: DOMAIN,
+    },
+    {
+      protocol: 'https',
+      hostname: '*.supabase.co', // Importante para que Astro procese las fotos del storage
+    },
+     {
+      protocol: 'https',
+      hostname: '*.vercel.app', // Importante para que Astro procese las fotos del storage
+    },
+  ],
+},
+
+  adapter: vercel(),
+  integrations: [icon()]
 });
